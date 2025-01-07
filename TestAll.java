13152076892f8d5566e73624b844ce76fd7258f8
GoBuLlying <guishere0z@gmail.com>
2025-01-08 03:57:07 +0800

    Update TestAll.java

--- TestAll.java
+++ TestAll.java
@@ -4,5 +4,7 @@ public class TestAll {
     public static void main(String[] args) {
         Test t= new Test();
         t.run();
+        String diceDetails = t.getDiceDetails(1); // 获取第一个骰子的详细信息
+        System.out.println(diceDetails);
     }
 }
